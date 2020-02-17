ce881b4814d8b5a06b9a01c4b2bf4f7a1e7a943c
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 17:15:05 +0600

    Even Number Printing

---
+++ While-Loop.py
@@ -0,0 +1,15 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+num1 = int(input("Enter starting number :"))
+num2 = int(input("Enter ending number :"))
+
+i = num1
+print("Even numbers are :")
+while i <= num2:
+    print(i)
+    i = i + 2
+
+
