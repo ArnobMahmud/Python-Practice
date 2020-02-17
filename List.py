86771fd091cb4ca3118b1ecb9c0cf353a9b4e5ed
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 19:05:41 +0600

    List Works

---
+++ List.py
@@ -0,0 +1,21 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+language = ["C", "Python", "Java", "SQL", "C++", "OS", "PHP"]
+
+print(len(language))
+
+print(language[3])
+
+print(language[2:])
+
+print("C#" in language)
+
+print("JS" not in language)
+
+
+
+
+
