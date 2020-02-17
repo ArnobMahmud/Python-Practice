ecfbd3f82ef843a108d2097832ceeddae075f3e3
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 15:25:19 +0600

    Circles Area

---
+++ Area-Of-Circle.py
@@ -0,0 +1,9 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+radius = float(input("Enter the value of radius :"))
+
+area = float(3.142 * pow(radius,2))
+print("Area of circle is :", area)
