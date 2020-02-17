eac806266ae3b02c956c5a735155c883ca4a56e8
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 17:50:26 +0600

    Sum of Even Numbers in Series

---
+++ Arithmetic-Series.py
@@ -0,0 +1,15 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+num = int(input("Enter last value :"))
+sum = 0
+
+i = 0
+while i <= num:
+    print(i)
+    sum = sum + i
+    i = i + 2
+
+print("Sum is :", sum)
