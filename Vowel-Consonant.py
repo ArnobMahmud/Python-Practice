c957e09899dd4f71e5ede21cbb9f993e565f8d97
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 17:03:14 +0600

    Alphabet Check

---
+++ Vowel-Consonant.py
@@ -0,0 +1,11 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+ch = str(input("Enter an alphabet :"))
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("This is a vowel.")
+else:
+    print("This is a consonant.")
