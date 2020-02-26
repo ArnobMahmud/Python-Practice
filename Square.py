0fcc1c489092f9d5655cd93768e847a7ca7dae86
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-26 12:47:48 +0600

    Square Draw

---
+++ Square.py
@@ -0,0 +1,26 @@
+'''
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+'''
+import turtle
+
+#square
+arnob_turtle = turtle.Turtle()
+
+def square():
+
+       arnob_turtle.forward(100)
+       arnob_turtle.right(90)
+       arnob_turtle.forward(100)
+       arnob_turtle.right(90)
+       arnob_turtle.forward(100)
+       arnob_turtle.right(90)
+       arnob_turtle.forward(100)
+       arnob_turtle.right(90)
+
+square()
+arnob_turtle.forward(150)
+arnob_turtle.right(90)
+square()
+
+
