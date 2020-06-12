f12eb554390ab88ad954234d118c5161c3ca4b4f
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-06-12 22:15:11 +0600

    Class

---
+++ Class.py
@@ -0,0 +1,15 @@
+'''
+       Author : Arnob Mahmud
+
+       Mail : arnob.tech.me@gmail.com
+'''
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Arnob", 19)
+
+print(p1.name)
+print(p1.age
