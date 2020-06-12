75374c5afe11e2d85f1efa015e811f845191d1e8
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-06-12 22:09:24 +0600

    Casting

---
+++ casting.py
@@ -0,0 +1,21 @@
+'''
+   Author : Arnob Mahmud
+
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+print(x)
+print(y)
+print(z)
+
+m = str("s1")
+n = str(2)
+o = str(3.0)
+
+print(m)
+print(n)
+print(o)
