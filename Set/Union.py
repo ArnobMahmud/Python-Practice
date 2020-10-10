3dce2de4c05db3e33ad5699f66785d7c73236c8d
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-10 16:20:42 +0600

    Set in Python

---
+++ Set/Union.py
@@ -0,0 +1,12 @@
+'''	
+	Author : Arnob Mahmud
+
+	Mail : arnob.tech.me @ gmail.com
+'''
+
+a = {1, 2, 3, 4, 'c', 'f', 9}
+b = {2, 3, 4, 7, 9, 'g', 'h', 'j', 'm'}
+c = {0, 2, 4, 5, 'd', 'e', 'w', 8}
+
+union = a.union(b,c)
+print(union)
