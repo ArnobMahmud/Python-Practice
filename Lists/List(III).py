eaae8d82a10bde6981ca95e565fa0852668c86fd
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-10 15:58:03 +0600

    List in Python

---
+++ Lists/List(III).py
@@ -0,0 +1,13 @@
+'''
+   Author : Arnob Mahmud
+
+   Mail : arnob.tech.me @ gmail.com
+'''
+
+list = [1, 3, 6, 7, 9, 12, ['Arnob', 'Mahmud', 3, 6], 66]
+print(list)
+
+print(list[6][0], list[6][1])
+
+list.remove(6)
+print(list)
